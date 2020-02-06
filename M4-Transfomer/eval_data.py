004ad8728a0a93f814e44e7f98d80c0510f38a4f
ToneLi <33856565+ToneLi@users.noreply.github.com>
2020-02-06 10:28:29 +0800

    Add files via upload

---
+++ M4-Transfomer/eval_data.py
@@ -0,0 +1,160 @@
+#encoding=utf-8
+
+import numpy as np
+
+class EvalData():
+    def __init__(self, config):
+        # super(EvalData, self).__init__(config)
+
+        self._eval_data_path = config["eval_data"]
+        self._output_path =  config["output_path"]
+        self._dic_path = config["char_dic"]
+        self._label_list_path = config["label_list"]
+
+        self._sequence_length = config["sequence_length"]  # 每条输入的序列处理为定长
+
+    def read_data(self):
+        """
+        读取数据
+        :return: 返回分词后的文本内容和标签，inputs = [[]], labels = []
+        """
+        inputs = []
+        labels = []
+        with open(self._eval_data_path, "r", encoding="utf8") as fr:
+            for line in fr.readlines():
+                try:
+                    text, label = line.strip().split("\t")
+                    inputs.append(text.strip().split(" "))
+                    labels.append(label)
+                except:
+                    continue
+
+        return inputs, labels
+
+
+    def gen_word_label_index(self):
+        """
+        生成词汇，标签等映射表
+        :param words: 训练集所含有的单词
+        :param labels: 标签
+        :return:
+        """
+        vocab = []
+        with open(self._dic_path, "r", encoding="utf-8") as fr:
+            for line in fr.readlines():
+                try:
+                    line = line.strip().split("\t")
+                    vocab.append(line[0])
+
+                except:
+                    continue
+
+        vocab = ["<PAD>", "<UNK>"] + vocab
+        self.vocab_size = len(vocab)
+        word_to_index = dict(zip(vocab, list(range(len(vocab)))))
+
+        labels = []
+        with open(self._label_list_path, "r", encoding="utf-8") as fr1:
+            for line1 in fr1.readlines():
+                try:
+                    line1 = line1.strip()
+                    labels.append(line1)
+
+                except:
+                    continue
+
+        label_to_index = dict(zip(labels, list(range(len(labels)))))
+
+        return word_to_index, label_to_index
+
+    @staticmethod
+    def trans_to_index(inputs, word_to_index):
+        """
+        将输入转化为索引表示
+        :param inputs: 输入
+        :param word_to_index: 词汇-索引映射表
+        :return:
+        """
+        inputs_idx = [[word_to_index.get(word, word_to_index["<UNK>"]) for word in sentence] for sentence in inputs]
+
+        return inputs_idx
+
+    @staticmethod  # do not need self anymore
+    def trans_label_to_index(labels, label_to_index):
+        """
+        将标签也转换成数字表示
+        :param labels: 标签
+        :param label_to_index: 标签-索引映射表
+        :return:
+        """
+        labels_idx = [label_to_index[label] for label in labels]
+        return labels_idx
+
+    def padding(self, inputs, sequence_length):
+        """
+        对序列进行截断和补全
+        :param inputs: 输入
+        :param sequence_length: 预定义的序列长度
+        :return:
+        """
+        new_inputs = [sentence[:sequence_length]
+                      if len(sentence) > sequence_length
+                      else sentence + [0] * (sequence_length - len(sentence))
+                      for sentence in inputs]
+
+        return new_inputs
+
+    def gen_data(self):
+        """
+        to make the data which model need
+        :return:
+        """
+
+        # 1，read the source text
+        inputs, labels = self.read_data()
+        print("read finished")
+
+        # 2. word label index
+        word_to_index, label_to_index = self.gen_word_label_index()
+        # print("word_to_index",word_to_index)
+        # print("label_to_index",label_to_index)
+        print("vocab process finished")
+        #
+        # 3，input to index
+        inputs_idx = self.trans_to_index(inputs, word_to_index)
+        # print((inputs_idx))
+        print("index transform finished")
+        #
+        # 4, padding
+        inputs_idx = self.padding(inputs_idx, self._sequence_length)
+        # print(inputs_idx)
+        print("padding finished")
+
+        # 6，label to index
+        labels_idx = self.trans_label_to_index(labels, label_to_index)
+        print("label index transform finished")
+
+        return np.array(inputs_idx), np.array(labels_idx)
+
+    def next_batch(self, x, y, batch_size):
+        """
+        生成batch数据集
+        :param x: 输入
+        :param y: 标签
+        :param batch_size: 批量的大小
+        :return:
+        """
+        perm = np.arange(len(x))
+        np.random.shuffle(perm)
+        x = x[perm]
+        y = y[perm]
+
+        num_batches = len(x) // batch_size
+
+        for i in range(num_batches):
+            start = i * batch_size
+            end = start + batch_size
+            batch_x = np.array(x[start: end], dtype="int64")
+            batch_y = np.array(y[start: end], dtype="float32")
+
+            yield dict(x=batch_x, y=batch_y)
---
+++ M4-Transfomer/predict.py
@@ -0,0 +1,133 @@
+#encoding=utf-8
+import os
+import json
+import datetime
+import tensorflow as tf
+from  transformer import  TransformerModel
+from eval_data import EvalData
+from metrics import get_multi_metrics,mean
+now_path=os.getcwd()
+
+class Predictor():
+    def __init__(self, args):
+        self.args = args
+        with open(os.path.join(now_path, args), "r") as fr:
+            config = json.load(fr)  #
+
+
+        self.model = None
+        self.config = config
+        self._dic_path = config["char_dic"]
+        self._label_list_path = config["label_list"]
+        self.word_to_index, self.label_to_index = self.gen_word_label_index()
+        self.index_to_label = {value: key for key, value in self.label_to_index.items()}
+        self.vocab_size = len(self.word_to_index)
+        self.word_vectors = None
+        self.sequence_length = self.config["sequence_length"]
+        self.eval_data_obj = EvalData(self.config)
+        # 创建模型
+        self.eval_inputs, self.eval_labels = self.eval_data_obj.gen_data()
+        self.create_model()
+        # 加载计算图
+        self.load_graph()
+
+
+    def gen_word_label_index(self):
+        """
+        生成词汇，标签等映射表
+        :param words: 训练集所含有的单词
+        :param labels: 标签
+        :return:
+        """
+        vocab = []
+        with open(self._dic_path, "r", encoding="utf-8") as fr:
+            for line in fr.readlines():
+                try:
+                    line = line.strip().split("\t")
+                    vocab.append(line[0])
+
+                except:
+                    continue
+
+        vocab = ["<PAD>", "<UNK>"] + vocab
+        self.vocab_size = len(vocab)
+        word_to_index = dict(zip(vocab, list(range(len(vocab)))))
+
+        labels = []
+        with open(self._label_list_path, "r", encoding="utf-8") as fr1:
+            for line1 in fr1.readlines():
+                try:
+                    line1 = line1.strip().split("\t")
+                    labels.append(line1[0])
+
+                except:
+                    continue
+
+        label_to_index = dict(zip(labels, list(range(len(labels)))))
+
+        return word_to_index, label_to_index
+
+
+
+
+    def sentence_to_idx(self, sentence):
+        """
+        将分词后的句子转换成idx表示
+        :param sentence:
+        :return:
+        """
+        sentence_ids = [self.word_to_index.get(token, self.word_to_index["<UNK>"]) for token in sentence]
+        sentence_pad = sentence_ids[: self.sequence_length] if len(sentence_ids) > self.sequence_length \
+            else sentence_ids + [0] * (self.sequence_length - len(sentence_ids))
+        return sentence_pad
+
+
+    def load_graph(self):
+        """
+        加载计算图
+        :return:
+        """
+        self.sess = tf.Session()
+        ckpt = tf.train.get_checkpoint_state(self.config["ckpt_model_path"])
+        if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
+            print('Reloading model parameters..')
+            self.model.saver.restore(self.sess, ckpt.model_checkpoint_path)
+        else:
+            raise ValueError('No such file:[{}]'.format(self.config["ckpt_model_path"]))
+
+
+    def create_model(self):
+        self.model = TransformerModel(config=self.config, vocab_size=self.vocab_size)
+
+
+
+    def predict(self):
+        eval_accs = []
+        label_list=[]
+        with open(self._label_list_path,"r",encoding="utf-8") as fr:
+            for line in fr.readlines():
+                label_list.append(line.strip())
+
+
+        for eval_batch in self.eval_data_obj.next_batch(self.eval_inputs, self.eval_labels,
+                                                        self.config["batch_size"]):
+            eval_summary, eval_loss, eval_predictions = self.model.eval(self.sess, eval_batch)
+
+
+
+            acc, recall, prec, f_beta = get_multi_metrics(pred_y=eval_predictions,
+                                                          true_y=eval_batch["y"],
+                                                          labels=label_list)
+            print("准确度是-----",acc)
+            eval_accs.append(acc)
+
+        print("--------the accuracy is--------:",mean(eval_accs))
+
+
+if __name__=="__main__":
+    begin=datetime.datetime.now()
+    args = "transformer_config.json"
+    predict=Predictor(args=args)
+    predict.predict()
+    end=datetime.datetime.now()
+    print("------The run time is----------:",(end-begin).seconds)
